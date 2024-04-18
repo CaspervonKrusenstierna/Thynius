2334e0e92bd1e0f3fb4e3f79486c5c121aa24e9d
Casper von Krusenstierna <136915966+CaspervonKrusenstierna@users.noreply.github.com>
2024-04-18 09:30:39 +0200

    Update ThemisDetector.cs

--- ThemisWeb/ThemisWeb.Server/Common/ThemisDetector.cs
+++ ThemisWeb/ThemisWeb.Server/Common/ThemisDetector.cs
@@ -7,39 +7,54 @@ using static ThemisWeb.Server.Common.ThemistTextConverter;
 
 namespace ThemisWeb.Server.Common
 {
-    public struct MomentOfInterest
+    public struct MomentOfInterest //This data is outputted to the teacher so the teacher can quickly navigate and look at the moment of interest. Example of this is a paste.
     {
         public string reason;
         public int Index;
     }
-    public struct InputsStatistic
+    public struct InputsStatistic // This statistic is outputted to the teacher. Example: "Editscore: 3".
     {
         public string infoType;
         public string info;
     }
-    public class ThemisDetectionData
+    public class ThemisDetectionData //Data thats interesting to the teacher or analyzer in question
     {
         public List<String> remarks;
         public List<MomentOfInterest> momentsOfInterest;
         public List<InputsStatistic> statistics;
     }
-    public class ThemisDetector
-    {
-        IUserTextRepository _userTextRepository;
-        ApplicationUser _user;
-        UserText _text;
-
+    
+    private struct InternalThemisDetectorInputsData{ //This is the data that the inputs analyzer outputs for further processing
+            int totalAddCharTime;
+            int addCharAmount;
+            int deleteCharAmount;
+            int deleteSectionAmount;
+            int extendedTypingBreaksCount;
+            int smallPasteCount;
+            int totalExternalPasteChars;
+            List<MomentOfInterest> momentsOfInterest;
+    }
+    private struct InternalThemisDetectorInputsProcessingData{ //Data used by the algo to process the inputs
+        UInt64 currSessionRelativeTime;
+        int previousAddCharRelativeTime;
+        string currRawContent;
+        IUserTextRepository userTextRepository;
+        ApplicationUser user;
+        UserText text;
+    }
+    private struct InternalThemisDetectorTextData{ //the final processed data.
         private float editScore;
         private float averageTypeSpeed;
-        private int detectionScore = 0;
-
-        private int smallPasteCount = 0;
-        private int totalExternalPasteChars = 0;
-        private List<MomentOfInterest> momentsOfInterest = new List<MomentOfInterest>();
-        private List<String> remarks = new List<String>();
-
-        string RawContent = "";
-        private async Task<bool> wasTextTypedByHandBefore(string currRawContent, string Text, UInt64 timePoint)
+        private int detectionScore;
+        private List<String> remarks;
+    }
+    public class ThemisDetector
+    {
+        private InternalThemisDetectorInputsData inputsData = new InternalThemisDetectorInputsData();
+        private InternalThemisDetectorInputsProcessingData inputsProcessingData  = new InternalThemisDetectorInputsProcessingData();
+        private InternalThemisDetectorTextData textData = new InternalThemisDetectorTextData();
+        
+        private async Task<bool> wasTextTypedByHandBefore(string Text, UInt64 timePoint)
         {
             return currRawContent.Contains(Text);
             if (!currRawContent.Contains(Text))
@@ -63,59 +78,44 @@ namespace ThemisWeb.Server.Common
             }
             return true;
         }
-        private async Task<bool> AnalyzeInputs(List<Input> inputs)
-        {
-            int previousAddCharRelativeTime = 0;
-            int totalAddCharTime = 0;
-            int addCharAmount = 0;
-            int deleteCharAmount = 0;
-            int deleteSectionAmount = 0;
-            int extendedTypingBreaksCount = 0;
 
-            UInt64 currSessionRelativeTime = 0;
-
-            for (int i = 0; inputs.Count() > i; i++)
-            {
-                Input input = inputs[i];
-                switch (input._ActionType)
+        private inline void onAdd(Input input){
+                int timeDiff = (int)input.relativeTimePointMs - previousAddCharRelativeTime;
+                if (timeDiff >= 60000)
                 {
-                    case ActionType.ADDCHAR:
-   
-                        int timeDiff = (int)input.relativeTimePointMs - previousAddCharRelativeTime;
-                        if (timeDiff >= 60000)
-                        {
-                            extendedTypingBreaksCount++;
-                        }
-                        else
-                        {
-                            addCharAmount++;
-                            totalAddCharTime += timeDiff;
-                        }
-                        break;
-
-                    case ActionType.DELETECHAR: deleteCharAmount++; break;
-
-                    case ActionType.DELETESELECTION: deleteSectionAmount++; break;
-
-                    case ActionType.PASTE:
-                        if(input.ActionContent.Length <= 10) // probably just pasting in some external one word or char that user could not type on their keyboard
-                        {
-                            smallPasteCount++;
-                            break;
-                        }
-                        bool wasTypedBefore = await wasTextTypedByHandBefore(RawContent, input.ActionContent, currSessionRelativeTime+input.relativeTimePointMs);
-                        if (!wasTypedBefore)
-                        {
-                            totalExternalPasteChars += input.ActionContent.Length;
-                            momentsOfInterest.Add(new MomentOfInterest{ Index=i, reason="PASTE"});
-                        }
-                        break;
-                case ActionType.SESSIONSTART:
-                    currSessionRelativeTime = input.relativeTimePointMs;
-                    break;
+                    extendedTypingBreaksCount++;
                 }
-                AdvanceInput(ref RawContent, input);
+                else
+                {
+                    addCharAmount++;
+                    totalAddCharTime += timeDiff;
+                }
+                break;
+        }
+        
+        private inline void onDelete(Input input){
+             deleteCharAmount++; 
+        }
+        
+        private inline void onPaste(Input input){
+            if(input.ActionContent.Length <= 10) // probably just pasting in some external one word or char that user could not type on their keyboard
+            {
+                smallPasteCount++;
+                break;
             }
+            bool wasTypedBefore = await wasTextTypedByHandBefore(RawContent, input.ActionContent, currSessionRelativeTime+input.relativeTimePointMs);
+            if (!wasTypedBefore)
+            {
+                totalExternalPasteChars += input.ActionContent.Length;
+                momentsOfInterest.Add(new MomentOfInterest{ Index=i, reason="PASTE"});
+            }
+        }
+        
+        private inline void onSessionStart(Input input){
+            currSessionRelativeTime = input.relativeTimePointMs;
+        }
+
+        private inline void processAverageTypeSpeed(){
             if(addCharAmount == 0)
             {
                 averageTypeSpeed = totalAddCharTime;
@@ -124,6 +124,8 @@ namespace ThemisWeb.Server.Common
             {
                 averageTypeSpeed = totalAddCharTime/1000/addCharAmount;
             }
+        }
+        private inlinevoid processEditScore(){
             if(deleteCharAmount == 0)
             {
                 editScore = 0;
@@ -132,28 +134,41 @@ namespace ThemisWeb.Server.Common
             {
                 editScore = addCharAmount / deleteCharAmount;
             }
-            return true;
         }
-        public int GetDetectionScore()
+        private inline void processAnalyzerData(){
+            processAverageTypeSpeed();
+            processEditScore();
+        }
+        
+        private async Task AnalyzeInputs(List<Input> inputs)
         {
-            float score = 0;
-            if(editScore <= 0.5)
+            for (int i = 0; inputs.Count() > i; i++)
             {
-                if(editScore <= 0.2)
+                Input input = inputs[i];
+                switch (input._ActionType)
                 {
-                    remarks.Add("Användaren reviderade sin text ovanligt lite.");
-                    score += 1;
+                    case ActionType.ADDCHAR: onAdd(input); break;
+
+                    case ActionType.DELETECHAR: onDelete(input); break;
+
+                    case ActionType.PASTE: onPaste(input); break;
+                        
+                    case ActionType.SESSIONSTART: onSessionStart(input); break;
                 }
-                else
-                {
-                    score += (float)0.5 - editScore;
-                }
+                AdvanceInput(ref RawContent, input);
             }
-            if(smallPasteCount >= 20)
+            processAnalyzerData();
+            return;
+        }
+
+        private inline int getRoundedDetectionScore(){
+            if(score >= 3)
             {
-                remarks.Add("Användaren uppvisade besynnerligt beteende i form av många små inklistringar. Totalt antal små inklistringar: " + smallPasteCount);
-                score += 1;
+                return 3;
             }
+            return (int)(score);
+        }
+        private inline void factorInPastes(){
             if(totalExternalPasteChars >= RawContent.Length / 10)
             {
                 score += 1;
@@ -167,19 +182,36 @@ namespace ThemisWeb.Server.Common
                     remarks.Add("Användaren har kopierat delar av sin text från externa källor." + smallPasteCount);
                 }
             }
-            if(score >= 3)
+        }
+        private inline void factorInSmallPastes(){
+            if(smallPasteCount >= 20)
             {
-                return 3;
+                remarks.Add("Användaren uppvisade besynnerligt beteende i form av många små inklistringar. Totalt antal små inklistringar: " + smallPasteCount);
+                score += 1;
             }
-            if(score >= 2)
+        }
+        private inline void factorInEditScore(){
+            if(editScore <= 0.5)
             {
-                return 2;
+                if(editScore <= 0.2)
+                {
+                    remarks.Add("Användaren reviderade sin text ovanligt lite.");
+                    score += 1;
+                }
+                else
+                {
+                    score += (float)0.5 - editScore;
+                }
             }
-            if(score >= 1)
-            {
-                return 1;
-            }
-            return 0;
+        }
+        
+        public int GetDetectionScore()
+        {
+            factorInEditScore();
+            factorInSmallPastes();
+            factorInPastes();
+            
+            return getRoundedDetectionScore();
         }
         private List<InputsStatistic> GetInputsStatistics()
         {
@@ -188,17 +220,13 @@ namespace ThemisWeb.Server.Common
             toReturn.Add(new InputsStatistic { infoType = "Ändrings poäng", info = editScore.ToString() });
             return toReturn;
         }
+        
         public ThemisDetectionData getDetectionData()
         {
             ThemisDetectionData toReturn = new ThemisDetectionData();
             toReturn.statistics = GetInputsStatistics();
             toReturn.momentsOfInterest = momentsOfInterest;
             toReturn.remarks = remarks;
-            return toReturn;
-        }
-        public string getDescription()
-        {
-            string toReturn = "";
             return toReturn;
         }
 
