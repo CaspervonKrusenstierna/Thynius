752ece9339320375d21232da2df32eae80e1cec0
CaspervonKrusenstierna <136915966+CaspervonKrusenstierna@users.noreply.github.com>
2025-02-12 10:26:49 +0100

    Cleaned up RegisterBox.js

--- ThyniusWeb/thyniusweb.client/src/pages/homepage/registerbox/RegisterBox.jsx
+++ ThyniusWeb/thyniusweb.client/src/pages/homepage/registerbox/RegisterBox.jsx
@@ -8,60 +8,41 @@ import { AnchorButton, Input, SubmitButton } from '../../../shared/components/ho
 import HomePageBoxHeader from '../../../shared/components/dashboard/homepageboxheader/HomePageBoxHeader';
 import { EnvelopeImg, UnlockImg } from '../../../shared/assets';
 import { useTranslation } from 'react-i18next';
+import { validateEmail } from './utils/validateMail';
 
-const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
 
 const RegisterBox = () => {
     const [t, i18] = useTranslation();
     const [error, setError] = useState();
-    const Username = useRef("");
-    const Password = useRef("");
-    const Email = useRef("");
-    const ConfirmPassword = useRef("");
+    const formValues = useRef({
+        password: '',
+        email: '',
+        confirmPassword: '',
+      });
     const navigate = useNavigate();
 
     async function onSubmit() {
-        let submittedPassword = Password.current;
-        if (submittedPassword != ConfirmPassword.current) {
+        let submittedForm = formValues.current;
+        if (submittedForm.password != submittedForm.confirmPassword) {
             setError(t("Your passwords dont match."))
             return;
         }
-        if (!validateEmail(Email.current)){
+        if (!validateEmail(submittedForm.email)){
             setError(t("Please submit a valid email address."));
             return;
         }
-        if (submittedPassword < 8){
-            setError(t("Your password needs to contain 8 characters at minimum."))
+        let passwordError = getPasswordError(submittedForm.password);
+        if(passwordError){
+            setError(t(passwordError));
             return;
         }
-        if (submittedPassword > 24){
-            setError(t("Your password cannot contain more than 24 characters."))
-            return;
-        }
-        if(!/\d/.test(submittedPassword)){
-            setError(t("Your password needs to contain one numerical character."))
-            return;
-        }
-        if(!/[A-Z]/.test(submittedPassword)){
-            setError(t("Your password needs to contain atleast one capital letter."))
-            return;
-        }
-        if(!/[a-z]/.test(submittedPassword)){
-            setError(t("Your password needs to contain atleast one lowercase letter."))
-            return;
-        }
-
-        const response = await useRegister(Password.current, Email.current);
+        const response = await useRegister(submittedForm.password, submittedForm.email);
         if (response.ok) {
             navigate("/")
         }
-        response.json().then(res => { setError(JSON.stringify(res.error).replaceAll('"', "")) })
+        response.json().then((res) => {
+            setError(res.error || t("An unexpected error occurred."));
+        });
     }
 
         return (
@@ -69,9 +50,9 @@ const RegisterBox = () => {
                 <HomePageBoxHeader></HomePageBoxHeader>
                 <div className='InputContainer'>
                     <p className="text-base text-red-500 min-h-5">{error}</p>
-                    <Input onChange={(e) => { Email.current = e.target.value }} label="Email" img={EnvelopeImg}></Input>
-                    <Input hide={true} onChange={(e) => { Password.current = e.target.value }} label={t("Password")} img={UnlockImg}></Input>
-                    <Input hide={true} onChange={(e) => { ConfirmPassword.current = e.target.value }} label={t("Confirm password")} img={UnlockImg}></Input>
+                    <Input onChange={(e) => { formValues.current.email = e.target.value }} label="Email" img={EnvelopeImg}></Input>
+                    <Input hide={true} onChange={(e) => { formValues.current.password = e.target.value }} label={t("Password")} img={UnlockImg}></Input>
+                    <Input hide={true} onChange={(e) => { formValues.current.confirmPassword = e.target.value }} label={t("Confirm password")} img={UnlockImg}></Input>
                     <div className='HomePageBox-BottomContainer'>
                         <AnchorButton text={t("Already have an account?")} onClick={() => { navigate("/login") }}></AnchorButton>
                     </div>
